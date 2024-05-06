a3481c6a805007f215967a836f31120af5177162
galeshayek <galeshayek15@gmail.com>
2024-05-06 16:43:50 +0300

    added page title and logo

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -2,10 +2,11 @@ import { useContext } from "react";
 import { useTranslation } from "react-i18next";
 import { langContext } from "../../contexts/langContext";
 import { ThemeContext } from "../../contexts/ThemeContext";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { lngs } from "../../translation/lngs";
 import { BiMoon, BiSun } from "react-icons/bi";
 import './style.scss'
+import logo from '../../assets/logo.png'
 
 const NavBar = () => {
     const { updateLang, lang } = useContext(langContext);
@@ -15,7 +16,11 @@ const NavBar = () => {
 
     return (
         <>
-            <ul className='lg:col-start-2 lg:col-end-2 flex flex-col lg:flex-row gap-4 lg:gap-0 text-primary lg:text-oposite justify-between items-center lg:items-end *:text-xl font-normal' dir={i18n.dir(lang)} >
+            <Link to={'/'} className="size-12 lg:col-start-1 lg:col-end-2 self-end place-self-center">
+                <img src={logo} alt="" />
+            </Link>
+
+            <ul className='lg:col-start-2 lg:col-end-3 flex flex-col lg:flex-row gap-4 lg:gap-0 text-primary lg:text-oposite justify-between items-center lg:items-end *:text-xl font-normal' dir={i18n.dir(lang)} >
                 <li>
                     <NavLink to="/" className={({ isActive, isPending }) =>
                         isPending ? "pending" : isActive ? "active" : ""
