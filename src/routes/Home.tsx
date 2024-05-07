4369d4f6322495d818b03e7abbe622f23c210e89
galeshayek <galeshayek15@gmail.com>
2024-05-07 22:22:12 +0300

    removed unused

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -2,7 +2,6 @@ import CenterOf from "../sections/CenterOf/CenterOf"
 import Hero from "../sections/Hero/Hero"
 import Numbers from "../sections/Numbers/Numbers"
 import WhoAmI from "../sections/WhoAmI/WhoAmI"
-import { postEmail } from "../services/axiosPost"
 
 const Home = () => {
     return (<div className="">
